ac7d41d1bcff90349ecda3e36c08e3cbba0086b3
Yusuf Akin <kutluhanakin@gmail.com>
2022-04-04 16:36:09 -0400

    assignment items

---
+++ day24java.js
@@ -0,0 +1,28 @@
+let nameinput = $("#name")
+let emailinput = $("#email")
+let phoneinput = $("#phone")
+
+$("#submit").on("click", () => {
+  alert('You submittted!');
+
+let required = [nameinput, emailinput, phoneinput]
+
+for(let inputs of required){
+  if ( inputs.val() == "" ){
+    $("#message").prev().text("Please Fill Out Required Field")
+    $("#message").prev().addClass("warning")
+     inputs.prev().addClass("warning")
+  } else { 
+    inputs.prev().removeClass("warning")
+
+}
+}
+
+if($("label").hasClass("warning")){
+} else {
+  $("#form").remove()
+  $("h2").text("Thanks for your feedback")
+}})
+
+
+
